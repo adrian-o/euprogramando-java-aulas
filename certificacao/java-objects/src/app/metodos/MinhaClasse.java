f0d79f817669a001eb28b3a146267e37b1384537
Adriano Ohana <adriano.ohana@gmail.com>
2021-03-25 19:06:53 -0300

    Atualizando as aulas
    Atualizando as aulas

--- certificacao/java-objects/src/app/metodos/MinhaClasse.java
+++ certificacao/java-objects/src/app/metodos/MinhaClasse.java
@@ -5,13 +5,30 @@ public class MinhaClasse {
     int numero;
     String cor;
 
+    public MinhaClasse() {
+        // new MinhaClasse(99, "Preta");
+        this(99, "Preta");
+    }
+
+    public MinhaClasse(int numero) {
+        this(numero, "Preta");
+    }
+
+//    public int MinhaClasse(int number) {
+//        return 0;
+//    }
+
+    public MinhaClasse(String cor) {
+        this(99, cor);
+    }
+
     public MinhaClasse(int numero, String cor) {
         this.numero = numero;
         this.cor = cor;
     }
 
     public static void main(String[] args) {
-        MinhaClasse mc = new MinhaClasse(10,"Branca");
+        MinhaClasse mc = new MinhaClasse();
         System.out.println("Valores - > " + mc.numero + " " + mc.cor);
     }
 }
